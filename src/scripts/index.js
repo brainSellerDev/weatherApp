f662f0b72aba0b91abb972a5510d3f406bac33bc
Brain Seller <kesamirov1997@gmail.com>
2023-07-31 20:20:40 +0500

    finish left side of App

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,6 @@
 const URL = 'http://api.weatherapi.com/v1'
 const KEY = '544e535e35644ffeb4c93434232107'
 const IP_KEY = '68b0fd85539e4558b75a061381a881ce'
-const city = ''
 const weekLongNameDays = [
 	'Воскресенье',
 	'Понедельник',
@@ -12,32 +11,46 @@ const weekLongNameDays = [
 	'Суббота'
 ]
 const months = [
-	'Январь',
-	'Февраль',
-	'Март',
-	'Апрель',
-	'Май',
-	'Июнь',
-	'Июль',
-	'Август',
-	'Сентябрь',
-	'Октябрь',
-	'Ноябрь',
-	'Декабрь'
+	'Января',
+	'Февраля',
+	'Марта',
+	'Апреля',
+	'Мая',
+	'Июня',
+	'Июля',
+	'Августа',
+	'Сентября',
+	'Октября',
+	'Ноября',
+	'Декабря'
 ]
 const weekShortNameDays = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
-
-const getWeekDay = (date, arr) => {
-	return arr[date.getDay()]
-}
-const getMonth = (date, arr) => {
-	return arr[date.getMonth()]
-}
+let ip = ''
+//App left side 
+const currentCity = document.querySelector('.weather-app-left-aside-geolocation-info__city')
+const weekDay = document.querySelector('.weather-app-left-aside__title') 
+const currentDate = document.querySelector('.weather-app-left-aside__date')
+const weatherForecastIcon = document.querySelector('.weather-app-left-aside-current-day-forecast__icon')
+const weatherTemperature = document.querySelector('.weather-app-left-aside-current-day-forecast__temperature')
+const weatherCondition = document.querySelector('.weather-app-left-aside-current-day-forecast__condition')
+// App right side 
+const precipitationValue = document.querySelector('.precipitation-value')
+const humidityValue = document.querySelector('.humidity-value')
+const windValue = document.querySelector('.wind-value')
 
 const date = new Date()
-const weekday = getWeekDay(date, weekLongNameDays)
+weekDay.innerHTML = getWeekDay(date, weekLongNameDays)
 const month = getMonth(date, months)
 const [day, , year] = date.toLocaleDateString().split('.')
+currentDate.innerHTML = `${day} ${month} ${year}`
+
+function getWeekDay (date, arr) {
+	return arr[date.getDay()]
+}
+
+function getMonth(date, arr){
+	return arr[date.getMonth()]
+}
 
 const getIpAddress = async key => {
 	try {
@@ -64,12 +77,19 @@ async function getRequest(...[type, city, days, ipAddress]) {
 // types = ip, current, forecast
 
 const render = async () => {
-	const ip = await getIpAddress(IP_KEY)
+	ip = await getIpAddress(IP_KEY)
 	//get city by ipAddress
-	const { city } = await getRequest('ip', ip)
+	const {city} = await getRequest('ip', ip)
+	currentCity.innerHTML = city
 	// get current cities weather forecast
-	// const todayWeatherForecast = await getRequest('current', city)
-	// console.log('current day forecast', todayWeatherForecast)
+	const todayWeatherForecast = await getRequest('current', city)
+	console.log('current day forecast', todayWeatherForecast)
+	weatherForecastIcon.style.backgroundImage = `url(${todayWeatherForecast?.current?.condition?.icon})`
+	weatherTemperature.innerHTML = `${todayWeatherForecast?.current?.temp_c} °C`
+	weatherCondition.innerHTML = todayWeatherForecast?.current?.condition?.text
+	precipitationValue.innerHTML = `${todayWeatherForecast?.current?.precip_in} %`
+	humidityValue.innerHTML = `${todayWeatherForecast?.current?.humidity} %`
+	windValue.innerHTML = `${todayWeatherForecast?.current?.wind_kph} km/h`
 	// get forecast for several days
 	// const severalDaysForecast = await getRequest('forecast', city, 5)
 	// console.log('Several days forecast', severalDaysForecast)
