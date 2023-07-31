ac7792fccd56d6f759b12526f822b9d777149121
Brain Seller <kesamirov1997@gmail.com>
2023-07-31 19:17:43 +0500

    finish app layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,43 @@
 const URL = 'http://api.weatherapi.com/v1'
 const KEY = '544e535e35644ffeb4c93434232107'
 const IP_KEY = '68b0fd85539e4558b75a061381a881ce'
+const city = ''
+const weekLongNameDays = [
+	'Воскресенье',
+	'Понедельник',
+	'Вторник',
+	'Среда',
+	'Четверг',
+	'Пятница',
+	'Суббота'
+]
+const months = [
+	'Январь',
+	'Февраль',
+	'Март',
+	'Апрель',
+	'Май',
+	'Июнь',
+	'Июль',
+	'Август',
+	'Сентябрь',
+	'Октябрь',
+	'Ноябрь',
+	'Декабрь'
+]
+const weekShortNameDays = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
+
+const getWeekDay = (date, arr) => {
+	return arr[date.getDay()]
+}
+const getMonth = (date, arr) => {
+	return arr[date.getMonth()]
+}
+
+const date = new Date()
+const weekday = getWeekDay(date, weekLongNameDays)
+const month = getMonth(date, months)
+const [day, , year] = date.toLocaleDateString().split('.')
 
 const getIpAddress = async key => {
 	try {
@@ -13,10 +50,10 @@ const getIpAddress = async key => {
 	}
 }
 
-async function getRequest(...[url, type, key, city, days, ipAddress]) {
+async function getRequest(...[type, city, days, ipAddress]) {
 	try {
 		const { data } = await axios(
-			`${url}/${type}.json?key=${key}&q=${city}&days=${days}&lang=ru`
+			`${URL}/${type}.json?key=${KEY}&q=${city}&days=${days}&lang=ru`
 		)
 		return data
 	} catch (error) {
@@ -28,11 +65,14 @@ async function getRequest(...[url, type, key, city, days, ipAddress]) {
 
 const render = async () => {
 	const ip = await getIpAddress(IP_KEY)
-	const { city } = await getRequest(URL, 'ip', KEY, ip)
-	const todayWeatherForecast = await getRequest(URL,  'current', KEY, city)
-	console.log('current', todayWeatherForecast)
-    const forecast = await getRequest(URL,  'forecast', KEY, city, 5 )
-	console.log('forecast',forecast)
+	//get city by ipAddress
+	const { city } = await getRequest('ip', ip)
+	// get current cities weather forecast
+	// const todayWeatherForecast = await getRequest('current', city)
+	// console.log('current day forecast', todayWeatherForecast)
+	// get forecast for several days
+	// const severalDaysForecast = await getRequest('forecast', city, 5)
+	// console.log('Several days forecast', severalDaysForecast)
 }
 
 render()
